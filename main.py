3f701a0b2b4969c24032e994f04ccdefde9a4fec
lmac02 <32623975+lmac02@users.noreply.github.com>
2018-03-07 22:40:32 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from person import *
-#from person import make_routingNumber
-#from person import make_ssn
-#import random as r
-#from datetime import datetime as dt
-from customer import customer
-from employee import employee
-from manager import manager
-from advisor import advisor
+from customer import *
+from employee import *
+from manager import *
+from advisor import *
+from teller import *
